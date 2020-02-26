d081657c39de3496a53c5f2437ee3ccc8e7cbd6e
colakvio <44584218+vtotolhua@users.noreply.github.com>
2020-02-25 22:37:05 -0600

    bdContactos
    Se modificó el script _ScrRcbInf, se agregó un txt de contactos separado por comas y se creó la Clase _classContInf para poder leer un documento txt separado por comas para que cargue la base de datos de clientes.

--- Assets/_Scripts/_ScrRcbInf.cs
+++ Assets/_Scripts/_ScrRcbInf.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
 
 public class _ScrRcbInf : MonoBehaviour
 {
     public TMP_InputField nvisit, ngAperson;
     private TMP_InputField.SubmitEvent escribe1, escribe2;
     public static string stgNvisit, stgNgAperson;
-
+    private string filepath = "Assets/_Contactos/listacontactos.txt";
+    
     private void Start()
     {
+        llenadoLista();
+
         if (gameObject.tag == "visita") {
             nvisit = this.gameObject.GetComponent<TMP_InputField>();
             escribe1 = new TMP_InputField.SubmitEvent();
@@ -26,13 +32,28 @@ public class _ScrRcbInf : MonoBehaviour
     }
 
     private void EnvInfvisit(string nombre1) {
-        stgNvisit = nombre1;
-       // Debug.Log("nombre visita " + stgNvisit);
+        stgNvisit = nombre1.ToLower();
     }
 
     private void EnvInfgAper(string nombre2)
     {
+        nombre2 = nombre2.ToLower();
         stgNgAperson = nombre2;
-       // Debug.Log("nombre gA " + stgNgAperson);
+        Debug.Log("nombre gA " + nombre2);
+    }
+
+    private void llenadoLista() {
+        string[] tomalineas = File.ReadAllLines(filepath);
+        List<_classContInf> contactosinfo = new List<_classContInf>();
+        for (int i = 0; i < tomalineas.Length; i++)
+        {
+            string[] nomfile = tomalineas[i].Split(',');
+            contactosinfo.Add(new _classContInf(nomfile[0], nomfile[1], nomfile[2]));
+        }
+
+        print("numero de filas" + contactosinfo.Count);
+        foreach (_classContInf contacto in contactosinfo) {
+            print(contacto.gAnombre + " " + contacto.telef + " " + contacto.correo );
+        }
     }
 }
---
+++ Assets/_Scripts/_classContInf.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _classContInf
+{
+    public string gAnombre;
+    public string telef;
+    public string correo;
+
+    public _classContInf(string gANombNue,
+                       string telNue,
+                       string corrNue) 
+    {
+        gAnombre = gANombNue;
+        telef = telNue;
+        correo = corrNue;
+    }
+}
