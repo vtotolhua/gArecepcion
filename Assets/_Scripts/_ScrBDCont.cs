0523f5f718d97ddd10c139c4442367088fbf3d69
colakvio <44584218+vtotolhua@users.noreply.github.com>
2020-03-24 20:37:56 -0600

    Cambios para actualizar
    Estoy organizando esta versión porque se corrompio y quiero dejarla al  punto como la tenía, leyendo desde la aplicación en android

--- Assets/_Scripts/_ScrBDCont.cs
+++ Assets/_Scripts/_ScrBDCont.cs
@@ -1,32 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
-
-public class _ScrBDCont : MonoBehaviour
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+public class _ScrBDCont : MonoBehaviour
 {
-    private string filepath = "Assets/_Contactos/listacontactos.txt";
-    public List<_classContInf> contactosinfo = new List<_classContInf>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        llenadoLista();    
-    }
-
-    private void llenadoLista()
-    {
-        string[] tomalineas = File.ReadAllLines(filepath);
-
-        for (int i = 0; i < tomalineas.Length; i++)
-        {
-            string[] nomfile = tomalineas[i].Split(',');
-            contactosinfo.Add(new _classContInf(nomfile[0], nomfile[1], nomfile[2]));
-        }
-/*        foreach (_classContInf contacto in contactosinfo)
-        {
-            print(contacto.gAnombre + " " + contacto.telef + " " + contacto.correo);
-        }
-*/
-    }
+    private TextAsset filepath;
+    public List<_classContInf> contactosinfo = new List<_classContInf>();
+    void Start()
+    {
+        llenadoLista();
+    }
+    private void llenadoLista()
+    {
+        TextAsset filepath = Resources.Load<TextAsset>("liscont");
+        string[] tomalineas = filepath.text.Split(';');
+        for (int i = 0; i < tomalineas.Length-1; i++)
+        {
+            print("linea " + i + " " + tomalineas[i]);
+            string[] nomfile = tomalineas[i].Split(',');
+            contactosinfo.Add(new _classContInf(nomfile[0], nomfile[1], nomfile[2]));
+                foreach (string elem in nomfile )
+                {
+                    print("elemento de lista " + elem);
+                }
+        }
+        foreach (_classContInf contacto in contactosinfo)
+        {
+            print("Holos " + contacto.gAnombre + " " + contacto.telef + " " + contacto.correo);
+        }
+    }
 }
