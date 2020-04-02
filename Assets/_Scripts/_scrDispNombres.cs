d6c9c987fa444e2e38ddaf7e70e698485013a0b7
colakvio <44584218+vtotolhua@users.noreply.github.com>
2020-04-02 15:00:23 -0600

    error de display no toma _classContInf 
    No me deja avanzar ya no toma el _classContInf y creo que fue porque creé un nuevo game object del gAperson

--- Assets/_Scripts/_scrDispNombres.cs
+++ Assets/_Scripts/_scrDispNombres.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
-using TMPro;
-using System.Collections;
-using System.Collections.Generic;
-using System;
-
-public class _scrDispNombres : MonoBehaviour
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+public class _scrDispNombres : MonoBehaviour
 {
+<<<<<<< Updated upstream
     /*************************************************************************************************/
     /*Esta clase nos permite mostrar los nombres que escribión el usuario*****************************/
     /*estos se muestran en la parte de la confirmación de los datos **********************************/
@@ -92,3 +92,54 @@ public class _scrDispNombres : MonoBehaviour
         yield return null;
     }
 }
+=======
+    /*************************************************************************************************/
+    /*Esta clase nos permite mostrar los nombres que escribión el usuario*****************************/
+    /*estos se muestran en la parte de la confirmación de los datos **********************************/
+    /*************************************************************************************************/
+    public  TMP_Text nvisita, ngAPerson;
+    private string stgvisita, stggAperson, temper;
+    public static string numcont, gacont;
+    public _ScrBDCont basecontactos;
+    public _classContInf listacont;
+    private List<string> nombres = new List<string>();
+    
+    void Start()
+    {
+        if (gameObject.tag == "visita") {
+            stgvisita = _ScrRcbInf.stgNvisit.ToString();
+            nvisita.text = stgvisita;
+            //Debug.Log("verifv " + stgvisita);
+        }
+        if (gameObject.tag == "gAPerson") {
+            StartCoroutine ("displnom");
+        }
+    }
+    IEnumerator displnom ()
+    {
+        //El nombre del colaborador gA
+        print ("hola");
+        stggAperson = _ScrRcbInf.stgNgAperson.ToString();
+        foreach (_classContInf contacto in basecontactos.contactosinfo){
+            temper = contacto.gAnombre;
+            if (temper.Contains(stggAperson))
+            {
+            //creamos lista que tiene nombres parecidos
+            nombres.Add(string.Format("{0}", temper)); 
+            //llenamos la lista con la clase que definimos
+            ngAPerson.text = temper;
+            gacont = temper;
+            numcont = contacto.telef;
+            //break;
+            }
+            else 
+            ngAPerson.text = "no encontramos a la persona";
+        }
+        for (int i = 0; i<nombres.Count; i++  )
+        {
+            print("nombres en lista " + nombres[i]); 
+        }
+        yield return null;
+    }
+}
+>>>>>>> Stashed changes
