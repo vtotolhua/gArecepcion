4dbdddbfb7b8239d7f0a60779c71f4904fa2a399
colakvio <44584218+vtotolhua@users.noreply.github.com>
2020-04-09 14:32:58 -0500

    Despliega nombres Coincide
    Se hicieron las modificaciones para que despliegue los nombres de las coincidencias. ahora vamos a poner el comportamiento de un botón para seleccionar.

--- Assets/_Scripts/_scrPanelCoin.cs
+++ Assets/_Scripts/_scrPanelCoin.cs
@@ -6,22 +6,36 @@ using UnityEngine.UIElements;
 
 public class _scrPanelCoin : MonoBehaviour
 {
-    private GameObject goContCoin;
-    [SerializeField] _scrDispNombres baseCoincidencias;
-    public _classContInf listacoinc;
-    public static string ganombre, gatelefono, gamail;
+    private GameObject goContCoin, panel;  // goContCoin objeto con BD coinciden    //panel espacio a instanciar los objetos
+    //private TextMeshProUGUI tmpganombre;
+    public GameObject dispContacto;  //objeto a instanciar
+    [SerializeField] _scrDispNombres baseCoincidencias;   // script donde están las coincidencias
+    public _classContInf listacoinc; //clase que tiene el formato de la tabla  
+    public static string ganombre, gatelefono, gamail; //variables que guardan los valores de cada fila de coincidencia
+    //[SerializeField] TMP_Text ganombrepro;
+    private Transform transpanelparent;  //transform del panel para que el objeto a instanciar se genere aquí
     //public  TMP_Text ngAPerson;
     void Start()
     {
+
+        //tomamos el transform del panel donde se instancian las coincidencias
+        panel = this.gameObject;
+        transpanelparent = panel.transform;
+
+        //Buscamos el Game Object que tiene el script y la BD de las coincidencias
+        //guardamos el tanto el objeto como el script en variables internas para facilitar uso
+        //Inicio la corrutina de generar las coincidencias 
         if (GameObject.FindWithTag("contCoinci") != null)
         {
             goContCoin = GameObject.FindWithTag("contCoinci");
             baseCoincidencias = goContCoin.GetComponent<_scrDispNombres>();
             StartCoroutine("spawnCoincidencias");
         }
-
     }
 
+    // Iniciamos la corrutina que busca las coincidencias en la BD de coincidencias 
+    //y guarda el nombre, correo y teléfono en una variable pública 
+    //instancia un objeto tipo con los datos de la coincidencia y lo despliega dentro del panel
     IEnumerator spawnCoincidencias ()
     {
         foreach (_classContInf coincidencia in baseCoincidencias.contCoincide)
@@ -29,8 +43,12 @@ public class _scrPanelCoin : MonoBehaviour
             ganombre = coincidencia.gAnombre;
             gatelefono = coincidencia.telef;
             gamail = coincidencia.correo;
-            //print("las coincidencias son " + "nombre "+ ganombre + " telefono " + gatelefono + " mail " + gamail);
-            
+//          ganombrepro.text = ganombre;
+
+            print("las coincidencias son " + "nombre "+ ganombre + " telefono " + gatelefono + " mail " + gamail);
+            GameObject dipnomcoin = Instantiate(dispContacto, panel.transform.position, panel.transform.rotation) as GameObject;
+            dipnomcoin.transform.SetParent(transpanelparent);
+            dipnomcoin.GetComponent<TMP_Text>().text = ganombre;
         }
         yield return null;
     }
